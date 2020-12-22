10a0bd8c57f95ea20028cfaa4b6e7a071803b532
Lars Veldscholte <lars@tuxplace.nl>
2020-12-23 00:19:11 +0100

    Fix code style (whitespace)

--- src/SerialLogger.cpp
+++ src/SerialLogger.cpp
@@ -14,33 +14,33 @@ void SerialLogger::log() {
 	lastTime = millis();
 
 	char buf[62];
-	sprintf(buf, "%f %3d %f %3d %f %f %f %f",
-			humidistat.getHumidity(),
-			humidistat.setpoint,
-			humidistat.getTemperature(),
-			humidistat.controlValue,
-			trs[0].readTemp(),
-			trs[1].readTemp(),
-			trs[2].readTemp(),
-			trs[3].readTemp()
-			);
+	sprintf(buf, "%f %3d %f %3d %f %f %f %f %f %f %f",
+	        humidistat.getHumidity(),
+	        humidistat.setpoint,
+	        humidistat.getTemperature(),
+	        humidistat.controlValue,
+	        trs[0].readTemp(),
+	        trs[1].readTemp(),
+	        trs[2].readTemp(),
+	        trs[3].readTemp()
+	);
 	Serial.println(buf);
 }
 
 void SerialLogger::update() {
 	// Listen for RDY signal
-	if(Serial.available()) {
+	if (Serial.available()) {
 		char buf[8];
 		size_t numRead = Serial.readBytesUntil('\r', buf, 7);
 		buf[numRead] = '\0';
 
-		if(strcmp(buf, "RDY") == 0) {
+		if (strcmp(buf, "RDY") == 0) {
 			// Print header and set ready state
 			Serial.println(header);
 			ready = true;
 		}
 	}
 
-	if(ready && (millis() - lastTime > interval))
+	if (ready && (millis() - lastTime > interval))
 		log();
 }
