c894d000807bb81deb3fcfe84269b1eb19e26d36
Lars Veldscholte <lars@tuxplace.nl>
2021-08-17 15:20:09 +0200

    [Refactor] Change c-style cast to static_cast

--- src/sensor/FlowSensor.cpp
+++ src/sensor/FlowSensor.cpp
@@ -4,7 +4,7 @@
 FlowSensor::FlowSensor(uint8_t pin) : pin(pin) {}
 
 double FlowSensor::readFlowrate() const {
-	double voltage = (double) analogRead(pin) / 1023 * 3.3;
+	double voltage = static_cast<double>(analogRead(pin)) / 1023 * 3.3;
 
 	// Calculate flowrate from voltage using polynomial approximation
 	return coeffs[0]*pow(voltage, 5)
