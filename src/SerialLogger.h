d68c81d338423c4e66597e509b99ffe4aeb60420
Lars Veldscholte <lars@tuxplace.nl>
2021-08-30 14:35:16 +0200

    SerialLogger: Use template specializations instead of overloading

--- src/SerialLogger.h
+++ src/SerialLogger.h
@@ -10,23 +10,28 @@
 
 /// Logs humidistat data over serial.
 /// \tparam Humidistat_t Either SingleHumidistat or CascadeHumidistat
-template <class Humidistat_t>
+template<class Humidistat_t>
 class SerialLogger {
 private:
 	const Humidistat_t &humidistat;
-	const Array<const ThermistorReader*, 4> trs;
+	const Array<const ThermistorReader *, 4> trs;
 
-	const char *header = "Time Humidity Setpoint Temperature ControlValue T0 T1 T2 T3 pTerm iTerm dTerm";
+	// Can't specialize constexpr...
+	static const char header[];
+
 	const uint16_t interval;    //!< Logging interval (in millis)
 	unsigned long lastTime = 0; //!< Last time line was written (in millis)
 	bool ready = false;
+
+	/// Write a line to serial
+	void log();
 public:
 	/// Constructor.
 	/// \param humidistat Pointer to a Humidistat instance
 	/// \param trs        Array of 4 pointers to ThermistorReader instances
 	/// \param interval   Logging interval (in ms)
-	explicit SerialLogger(const Humidistat_t *humidistat, Array<const ThermistorReader*, 4> trs, uint16_t interval)
-		: humidistat(*humidistat), trs(trs), interval(interval) {}
+	explicit SerialLogger(const Humidistat_t *humidistat, Array<const ThermistorReader *, 4> trs, uint16_t interval)
+			: humidistat(*humidistat), trs(trs), interval(interval) {}
 
 	/// Setup the serial interface
 	static void begin(uint32_t baud) {
@@ -35,36 +40,12 @@ public:
 		Serial.println("RDY");
 	}
 
-	/// Write a line to serial
-	void log(const SingleHumidistat *humidistat) {
-		double pTerm, iTerm, dTerm;
-		humidistat->getTerms(pTerm, iTerm, dTerm);
-
-		char *buf = asprintf("%lu %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f",
-		                     lastTime,
-		                     humidistat->getHumidity(),
-		                     humidistat->sp,
-		                     humidistat->getTemperature(),
-		                     humidistat->cv,
-		                     trs[0] ? trs[0]->readTemp() : NAN,
-		                     trs[1] ? trs[1]->readTemp() : NAN,
-		                     trs[2] ? trs[2]->readTemp() : NAN,
-		                     trs[3] ? trs[3]->readTemp() : NAN,
-		                     pTerm,
-		                     iTerm,
-		                     dTerm
-		);
-
-		Serial.println(buf);
-		delete buf;
-	}
-
 	/// Log a line every interval, once data has been received
 	void update() {
 		if (ready) {
 			if (millis() - lastTime >= interval) {
 				lastTime = millis();
-				log(&humidistat);
+				log();
 			}
 			return;
 		}
@@ -83,5 +64,32 @@ public:
 		}
 	}
 };
+
+template<>
+const char SerialLogger<SingleHumidistat>::header[] = "Time Humidity Setpoint Temperature ControlValue T0 T1 T2 T3 pTerm iTerm dTerm";
+
+template<>
+void SerialLogger<SingleHumidistat>::log() {
+	double pTerm, iTerm, dTerm;
+	humidistat.getTerms(pTerm, iTerm, dTerm);
+
+	char *buf = asprintf("%lu %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f",
+	                     lastTime,
+	                     humidistat.getHumidity(),
+	                     humidistat.sp,
+	                     humidistat.getTemperature(),
+	                     humidistat.cv,
+	                     trs[0] ? trs[0]->readTemp() : NAN,
+	                     trs[1] ? trs[1]->readTemp() : NAN,
+	                     trs[2] ? trs[2]->readTemp() : NAN,
+	                     trs[3] ? trs[3]->readTemp() : NAN,
+	                     pTerm,
+	                     iTerm,
+	                     dTerm
+	);
+
+	Serial.println(buf);
+	delete buf;
+}
 
 #endif //HUMIDISTAT_SERIALLOGGER_H
