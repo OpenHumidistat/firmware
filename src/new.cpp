9d6b06e6aab024452c402b9f279f4b29d1e24859
Lars Veldscholte <lars@tuxplace.nl>
2021-10-07 14:03:36 +0200

    new.cpp: put include inside #if

--- src/new.cpp
+++ src/new.cpp
@@ -1,6 +1,6 @@
+#if __cplusplus >= 201402L && ARDUINO_AVR_UNO
 #include <stdlib.h>
 
-#if __cplusplus >= 201402L && ARDUINO_AVR_UNO
 void operator delete(void* ptr, size_t size) noexcept {
 	operator delete(ptr);
 }
