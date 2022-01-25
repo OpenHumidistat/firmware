0c33771b07f5498006bdb7431ac8a319b105ff12
Lars Veldscholte <lars@tuxplace.nl>
2022-01-25 15:55:39 +0100

    advanceEnum: support reverse

--- src/advanceEnum.h
+++ src/advanceEnum.h
@@ -1,12 +1,15 @@
 #ifndef HUMIDISTAT_ADVANCEENUM_H
 #define HUMIDISTAT_ADVANCEENUM_H
 
+#include <stdint.h>
+
 /// Cycle through (advance) an enum.
 /// \tparam T Enum type (should have _last as final element, equaling the last valid element)
 /// \param e Enum instance
+/// \param n Step (can be negative)
 template <typename T>
-void advanceEnum(T &e) {
-	e = static_cast<T>((static_cast<int>(e) + 1) % (static_cast<int>(T::_last) + 1));
+void advanceEnum(T &e, int8_t n = 1) {
+	e = static_cast<T>((static_cast<typename std::underlying_type<T>::type>(e) + n) % (static_cast<typename std::underlying_type<T>::type>(T::_last) + 1));
 }
 
 #endif //HUMIDISTAT_ADVANCEENUM_H
